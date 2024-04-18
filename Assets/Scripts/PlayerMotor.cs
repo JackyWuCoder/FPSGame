21ec321e31bac7877c6199942fc435aea30de13e
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-17 22:56:13 -0700

    Adding Gravity

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,7 +6,9 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private bool isGrounded;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float gravity = -9.8f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = controller.isGrounded;
     }
 
     // Receive the inputs for our InputManager.cs and apply them to our character controller
@@ -27,5 +29,12 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.x = input.x;
         moveDirection.z = input.y;
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
     }
 }
