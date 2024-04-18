62a9d227c7a4f0a9e6c17811f5f99df1493bfa64
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-17 22:35:59 -0700

    PlayerMotor and InputManager
    1) PlayerMotor uses the CharacterController to control the player movements. It has a ProcessMove() method that takes in a Vector2 corresponding to player input.
    2) InputManager has a field of PlayerMotor and passes the Vector2 movement input to the PlayerMotor object's ProcessMove method.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,23 +9,19 @@ public class InputManager : MonoBehaviour
     private PlayerInput playerInput;
     // reference to the OnFoot Action Map
     private PlayerInput.OnFootActions onFoot;
+    private PlayerMotor motor;
 
     private void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Tell the player motor to move using the value from our movement action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
     }
 
     private void OnEnable()
