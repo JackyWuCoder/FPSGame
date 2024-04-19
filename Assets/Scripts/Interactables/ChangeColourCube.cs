6d0dfe8304ccbbdcfa7060fcff0c5fa46ef168b5
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-18 23:10:58 -0700

    Implemented ChangeColour Cube

--- Assets/Scripts/Interactables/ChangeColourCube.cs
+++ Assets/Scripts/Interactables/ChangeColourCube.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeColourCube : MonoBehaviour
+public class ChangeColourCube : Interactable
 {
+    private MeshRenderer mesh;
+    [SerializeField] private Color[] colours;
+    private int colourIndex; // default value is 0
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = GetComponent<MeshRenderer>();
+        mesh.material.color = Color.red;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Interact()
     {
-        
+        colourIndex++;
+        if (colourIndex >= colours.Length)
+        {
+            colourIndex = 0;
+        }
+        mesh.material.color = colours[colourIndex];
     }
 }
