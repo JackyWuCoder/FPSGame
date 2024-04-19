9e8d4aea58a22606a7ed9100dfe8cd56be4342db
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-18 20:58:28 -0700

    Created an Interactable Script
    1) This script is an Abstract Class
    2) It uses the Template Method Pattern.
    3) This script should be inherited by any game objects that wants to become interactable.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    // Message displayed to player when looking at an interactable
+    [SerializeField] private string promptMessage;
+
+    // This function will be called from our player
+    public void BaseInteract()
+    {
+        Interact();
+    }
+
+    protected virtual void Interact()
+    { 
+        // We wont have any code written in this function
+        // this is a template funcction to be overriden by our subclasses
+    }
+}
