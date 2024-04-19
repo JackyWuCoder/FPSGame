676ddec595013446cc89beffc390be2d8b97c9d2
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-18 23:37:01 -0700

    Implemented CollectCube
    1) CollectCube Script inherits from Interactable.
    2) The particle effect from interaction scale change.

--- Assets/Scripts/Interactables/CollectCube.cs
+++ Assets/Scripts/Interactables/CollectCube.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectCube : MonoBehaviour
+public class CollectCube : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject particle;
+
+    private void Start()
     {
-        
+        GetComponent<MeshRenderer>().material.color = Color.yellow;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Interact()
     {
-        
+        Destroy(gameObject);
+        Instantiate(particle, transform.position, Quaternion.identity);
     }
 }
